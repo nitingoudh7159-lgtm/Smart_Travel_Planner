1ef2c1358fbeda60ff9e8ef0668b30a08abb924a
nitingoudh7159-lgtm <nitingoudh7159@gmail.com>
2025-12-19 10:07:17 +0530

    day 3 done

--- src/JS/destination.js
+++ src/JS/destination.js
@@ -4,8 +4,55 @@ const city = params.get("city");
 
 const cityName = document.getElementById("cityName");
 
-if(city){
+let GEODB_API_KEY = '74ea777f78msh3aef2974df38ec1p168482jsnbb0cf0a62764';
+
+let GEODB_HOST = 'wft-geo-db.p.rapidapi.com';
+
+if (city) {
     cityName.textContent = city;
-}else{
+
+    async function fetchCityDetials(cityName) {
+        try {
+            let res = await fetch(
+                `https://${GEODB_HOST}/v1/geo/cities?namePrefix=${city}&limit=1`,
+                {
+                    method: "GET",
+                    headers: {
+                        "X-RapidAPI-Key": GEODB_API_KEY,
+                        "X-RapidAPI-Host": GEODB_HOST,
+                    },
+                }
+            );
+            console.log(res);
+
+            if (!res.ok) {
+                console.log("API Response Not Fetched...");
+            }
+
+            let data = await res.json();
+            console.log(data);
+
+            if (!data.data || data.data.length === 0) {
+                console.log("City Not Found");
+            }
+
+            let cityData = data.data[0];
+
+            // Display City Info
+
+
+
+
+
+
+
+        } catch (error) {
+            console.log(error);
+
+        }
+    }
+    fetchCityDetials(city);
+} else {
     cityName.textContent = "Unknown city"
 }
+
