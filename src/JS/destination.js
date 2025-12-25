aa3059a6bd538ce5f329b85fd94c417128a0777e
nitingoudh7159-lgtm <nitingoudh7159@gmail.com>
2025-12-25 10:13:45 +0530

    done day 6

--- src/JS/destination.js
+++ src/JS/destination.js
@@ -77,3 +77,4 @@ if (city) {
 }
 
 document.querySelector("#weatherBtn").href = `weather.html?city=${city}`;
+document.querySelector("#placesBtn").href = `places.html?city=${city}`;
--- src/JS/weather.js
+++ src/JS/weather.js
@@ -7,6 +7,14 @@ const city = params.get("city");
 const cityName = document.querySelector("#cityName");
 const loading = document.querySelector("#loading");
 const errorBox = document.querySelector("#error");
+
+const weatherBox = document.querySelector("#weatherBox");
+const temp = document.querySelector("#temp");
+const feelsLike = document.querySelector("#feels");
+const condition = document.querySelector("#desc");
+const humidity = document.querySelector("#humidity");
+const windSpeed = document.querySelector("#wind");
+
 let API_KEY = "1d2b2cf941032f02ceb46afd63a78ed0";
 
 if(!city){
@@ -29,6 +37,31 @@ if(!city){
        
        let {lat, lon} = geoData[0];
        console.log(lat, lon);
+
+       const weatherRes = await fetch (`
+        https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`);
+        console.log(weatherRes);
+        if(!weatherRes.ok) throw new Error("Weather API failed")
+        
+        const weatherData = await weatherRes.json()
+        console.log(weatherData);
+        
+        temp.textContent = weatherData.main.temp;
+        feelsLike.textContent = weatherData.main.feels_like;
+        condition.textContent = weatherData.weather[0].description;
+        humidity.textContent = weatherData.main.humidity;
+        windSpeed.textContent = weatherData.wind.speed;
+
+
+        loading.classList.add("hidden");
+        weatherBox.classList.remove("hidden")
+
+
+
+
+
+
+
        
     } catch (error) {
         console.log(error, "Error in Fetching Weather Detials");
